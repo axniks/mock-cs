16dac7eac4d9112ea975fa7c5c1f5a1b2a570ea0
Axel Niklasson <axel.niklasson@gmail.com>
2021-10-05 15:35:30 +0100

    Add base xcode project

---
+++ mock-cs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mock-cs
+//
+//  Created by Axel Niklasson on 05/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
