e1c1d39241941c6cc9b4c27fa725453d1cfb3bcf
Axel Niklasson <axel.niklasson@gmail.com>
2021-10-12 17:25:02 +0100

    Tidy up, rename for clarity

--- mock-cs/ScoreView.swift
+++ mock-cs/ScoreView.swift
@@ -7,45 +7,61 @@
 
 import UIKit
 
-@IBDesignable class ScoreView: UIView {
-
+class ScoreView: UIView {
 
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var maxScoreLabel: UILabel!
     @IBOutlet weak var progressView: UIView!
 
-    private let scoreArcLayer = CAShapeLayer()
+    /// properties
+    private let progressIndicatorLayer = CAShapeLayer()
+    var progressIndicatorColor: UIColor = .orange
+    var progressIndicatorWidth: CGFloat = 4.0
+    var inset: CGPoint = CGPoint(x: 2.0, y: 2.0)
 
-    private func setupScoreArcLayer() {
+    var score: (score: Int, maxScore: Int) = (0, 0) {
+        didSet {
+            scoreLabel?.text = "\(score.score)"
+            maxScoreLabel?.text = "out of \(score.maxScore)"
 
-        /// add sublayer to show progress towards max score
-        progressView.layer.addSublayer(scoreArcLayer)
+            /// only animate progress if it is valid
+            if score.maxScore != 0 {
+                animateProgressIndicator()
+            }
+        }
+    }
 
-        /// centre layer in view
-        scoreArcLayer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-        scoreArcLayer.position = CGPoint(x: progressView.layer.bounds.midX, y: progressView.layer.bounds.midY)
+    /// sublayer in which to draw animated progress indicator
+    private func setupProgressIndicatorLayer() {
 
-        /// no fill, orange color, line width from property and round caps
-        scoreArcLayer.fillColor = nil
-        scoreArcLayer.strokeColor = UIColor.orange.cgColor
-        scoreArcLayer.lineWidth = CGFloat(scoreArcWidth)
-        scoreArcLayer.lineCap = .round
+        /// add progress sublayer to view layer
+        progressView.layer.addSublayer(progressIndicatorLayer)
+
+        /// centre progress layer in view layer
+        progressIndicatorLayer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        progressIndicatorLayer.position = CGPoint(x: progressView.layer.bounds.midX, y: progressView.layer.bounds.midY)
+
+        /// no fill, color and line width from properties and round caps
+        progressIndicatorLayer.fillColor = nil
+        progressIndicatorLayer.strokeColor = progressIndicatorColor.cgColor
+        progressIndicatorLayer.lineWidth = CGFloat(progressIndicatorWidth)
+        progressIndicatorLayer.lineCap = .round
 
         /// create path for circle with start point "up" and centered in layer
-        let radius = layer.bounds.width / 2 - scoreArcWidth - inset.x
-        scoreArcLayer.path = UIBezierPath(arcCenter: CGPoint(x: scoreArcLayer.bounds.width/2.0, y: scoreArcLayer.bounds.height/2.0), radius: radius, startAngle: (-0.5) * .pi, endAngle: 1.5 * .pi, clockwise: true).cgPath
+        let radius = layer.bounds.width / 2 - progressIndicatorWidth - inset.x
+        progressIndicatorLayer.path = UIBezierPath(arcCenter: CGPoint(x: progressIndicatorLayer.bounds.width/2.0, y: progressIndicatorLayer.bounds.height/2.0), radius: radius, startAngle: (-0.5) * .pi, endAngle: 1.5 * .pi, clockwise: true).cgPath
 
         /// do not stroke it yet
-        scoreArcLayer.strokeStart = 0.0
-        scoreArcLayer.strokeEnd = 0.0
+        progressIndicatorLayer.strokeStart = 0.0
+        progressIndicatorLayer.strokeEnd = 0.0
     }
 
     override func layoutSubviews() {
         super.layoutSubviews()
-        setupScoreArcLayer()
+        setupProgressIndicatorLayer()
     }
 
-    private func animateProgress() {
+    private func animateProgressIndicator() {
         /// A - slow animation to new value
         let grow = CABasicAnimation(keyPath: "strokeEnd")
         grow.toValue = CGFloat(score.score)/CGFloat(score.maxScore)
@@ -56,7 +72,7 @@ import UIKit
         /// B - near instant expansion of width
         let expand = CABasicAnimation(keyPath: "lineWidth")
         expand.beginTime = 0.5
-        expand.toValue = scoreArcWidth * 1.5
+        expand.toValue = progressIndicatorWidth * 1.5
         expand.duration = 0.01
         expand.fillMode = .forwards
         expand.isRemovedOnCompletion = false
@@ -80,7 +96,7 @@ import UIKit
         /// C - slowly back to original width
         let shrink = CABasicAnimation(keyPath: "lineWidth")
         shrink.beginTime = 0.51
-        shrink.toValue = scoreArcWidth
+        shrink.toValue = progressIndicatorWidth
         shrink.duration = 0.4
         shrink.fillMode = .forwards
         shrink.isRemovedOnCompletion = false
@@ -91,28 +107,6 @@ import UIKit
         growAndExpand.duration = 4.0
         growAndExpand.fillMode = .forwards
         growAndExpand.isRemovedOnCompletion = false
-        scoreArcLayer.add(growAndExpand, forKey: "line")
+        progressIndicatorLayer.add(growAndExpand, forKey: "line")
     }
-
-    var score: (score: Int, maxScore: Int) = (0, 0) {
-        didSet {
-            scoreLabel?.text = "\(score.score)"
-            maxScoreLabel?.text = "out of \(score.maxScore)"
-
-            /// only animate progress if it is valid
-            if score.maxScore != 0 {
-                animateProgress()
-            }
-        }
-    }
-
-    @IBInspectable
-    var scoreArcColor: UIColor = .orange
-
-    @IBInspectable
-    var scoreArcWidth: CGFloat = 4.0
-
-    @IBInspectable
-    var inset: CGPoint = CGPoint(x: 2.0, y: 2.0)
-
 }
